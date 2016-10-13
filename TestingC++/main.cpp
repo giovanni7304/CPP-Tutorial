be326558d73520bba8589fce00272efc5d297142
Terry Johnson <giovanni7304@me.com>
2016-10-13 18:13:29 -0500

    Initial Commit

--- TestingC++/main.cpp
+++ TestingC++/main.cpp
@@ -7,9 +7,21 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
 int main(int argc, const char * argv[]) {
+    int year = 2016, mon = 10, day = 12;
+    
+    
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    std::cout << "Today is : "
+    << std::setw (2) << std::setfill ('0') << day
+    << "."
+    << std::setw (2) << std::setfill ('0') << mon
+    << "."
+    << year << ".\n" << std::endl;
+    
     return 0;
 }
